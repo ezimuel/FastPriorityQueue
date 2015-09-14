0d91d5ced80e2014145775f0485ac4cd14ad6e4f
Emanuele Minotto <minottoemanuele@gmail.com>
2015-09-14 10:08:17 +0100

    docblocks

--- src/PriorityQueue.php
+++ src/PriorityQueue.php
@@ -1,21 +1,55 @@
 <?php
 
+namespace FastPriorityQueue;
+
 /**
  * Fast integer priority queue implementation using ordered arrays.
  *
  * @author Enrico Zimuel (enrico@zimuel.it)
  */
-
-namespace FastPriorityQueue;
-
 class PriorityQueue implements \Iterator, \Countable
 {
+    /**
+     * Queue elements (sorted by priority).
+     * 
+     * @var array
+     */
     protected $values = [];
+
+    /**
+     * Priorities.
+     * 
+     * @var array
+     */
     protected $priorities = [];
+
+    /**
+     * Maximum priority contained.
+     * 
+     * @var int
+     */
     protected $max = 0;
+
+    /**
+     * Total elements contained.
+     * 
+     * @var int
+     */
     protected $tot = 0;
+
+    /**
+     * Current index while looping the queue.
+     * 
+     * @var int
+     */
     protected $index = 0;
 
+    /**
+     * Insert a new element into the queue.
+     * 
+     * @param mixed $value    Element to insert.
+     * @param int   $priority Priority from 1 to PHP_INT_MAX.
+     */
     public function insert($value, $priority)
     {
         if (!is_int($priority) || $priority < 1) {
@@ -29,6 +63,11 @@ class PriorityQueue implements \Iterator, \Countable
         ++$this->tot;
     }
 
+    /**
+     * Extracts a node from the current position of the queue.
+     * 
+     * @return mixed
+     */
     public function extract()
     {
         if (!$this->valid()) {
@@ -40,21 +79,39 @@ class PriorityQueue implements \Iterator, \Countable
         return $value;
     }
 
+    /**
+     * Number of elements contained in the queue.
+     * 
+     * @return int
+     */
     public function count()
     {
         return $this->tot;
     }
 
+    /**
+     * Current element.
+     * 
+     * @return mixed
+     */
     public function current()
     {
         return current($this->values[$this->max]);
     }
 
+    /**
+     * Current element index.
+     * 
+     * @return int
+     */
     public function key()
     {
         return $this->index;
     }
 
+    /**
+     * Next element on the queue.
+     */
     public function next()
     {
         if (false === next($this->values[$this->max])) {
@@ -66,11 +123,19 @@ class PriorityQueue implements \Iterator, \Countable
         --$this->tot;
     }
 
+    /**
+     * Checks if the element in the end of the queue still exists.
+     * 
+     * @return bool
+     */
     public function valid()
     {
         return isset($this->values[$this->max]);
     }
 
+    /**
+     * Rewind iterator back to the start (no-op).
+     */
     public function rewind()
     {
         // No operation here, never moves from the top of the priority queue
