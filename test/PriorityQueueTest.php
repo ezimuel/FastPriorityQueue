ceb3403bf6f7c365af184ebdb34ea08dbe88d2be
Zero <john.royer@gmail.com>
2019-11-18 15:37:04 +0800

    Use PHPUnit 8 syntax

--- test/PriorityQueueTest.php
+++ test/PriorityQueueTest.php
@@ -2,8 +2,9 @@
 namespace Test;
 
 use FastPriorityQueue\PriorityQueue;
+use PHPUnit\Framework\TestCase;
 
-class PriorityQueueTest extends \PHPUnit_Framework_TestCase
+class PriorityQueueTest extends TestCase
 {
     public function setUp()
     {
